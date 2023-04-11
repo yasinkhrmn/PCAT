9939c1712fe802f94be2c4fb97a0fb21de823b94
yasinkhrmn <yasinkahrmaan271828@gmail.com>
2023-04-11 17:16:11 +0300

    Installed Express, Nodemon

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const express = require("express");
 
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("merhaba");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`sunucu ${port} portunda başlatildi.`);
+});
